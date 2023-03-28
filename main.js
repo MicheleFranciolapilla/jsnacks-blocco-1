631479dde12bb9078a3f680053a9a29c0d4edd06
MicheleFranciolapilla <michele.franciolapilla@gmail.com>
2023-03-28 18:11:03 +0900

    snack 2

--- main.js
+++ main.js
@@ -1,13 +1,31 @@
-let a = prompt("inserisci il primo numero");
-let b = prompt("inserisci il secondo numero");
+// SNACK 1
+// let a = prompt("inserisci il primo numero");
+// let b = prompt("inserisci il secondo numero");
 
-if (a > b)
-{
-    document.writeln(`Il numero piu' grande e' ${a}`);
-}
-else if (b > a)
-{
-    document.writeln(`Il numero piu' grande e' ${b}`);
-}
-else
-document.writeln(`I due numeri sono uguali`);
+// if (a > b)
+// {
+//     document.writeln(`Il numero piu' grande e' ${a}`);
+// }
+// else if (b > a)
+// {
+//     document.writeln(`Il numero piu' grande e' ${b}`);
+// }
+// else
+// document.writeln(`I due numeri sono uguali`);
+
+
+
+// SNACK 2
+
+ let p1 = prompt("prima parola");
+ let p2 = prompt("seconda parola");
+
+ if (p1.length > p2.length)
+ {
+    console.log(p2, " ", p1);
+ }
+
+ else
+ {
+    console.log(p1, " ", p2);
+ }
