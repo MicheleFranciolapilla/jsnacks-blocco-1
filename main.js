d464b3dd2c4fac680ca3e2ba3edb98161387beec
MicheleFranciolapilla <michele.franciolapilla@gmail.com>
2023-03-28 18:23:14 +0900

    snack 3

--- main.js
+++ main.js
@@ -15,17 +15,31 @@
 
 
 
-// SNACK 2
+// // SNACK 2
 
- let p1 = prompt("prima parola");
- let p2 = prompt("seconda parola");
+//  let p1 = prompt("prima parola");
+//  let p2 = prompt("seconda parola");
 
- if (p1.length > p2.length)
- {
-    console.log(p2, " ", p1);
- }
+//  if (p1.length > p2.length)
+//  {
+//     console.log(p2, " ", p1);
+//  }
 
- else
- {
-    console.log(p1, " ", p2);
- }
+//  else
+//  {
+//     console.log(p1, " ", p2);
+//  }
+
+
+// SNACK 3
+
+
+let somma = 0;
+
+for (let i=1; i <= 10; i++)
+{
+    let n = prompt("inserisci un numero");
+    somma += parseFloat(n);
+}
+
+console.log(somma);
